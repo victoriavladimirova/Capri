e6c94ec1b0b1ea30a76d80cdc5100161b9884b85
Victoria Vladimirova <victoriavladimirova@Victorias-MacBook-Pro.local>
2024-07-29 23:23:25 +0300

    Creating Capri project

--- 1.Homepage.js
+++
@@ -1,3 +0,0 @@
-var mobile = document.createElement('div');
-mobile.className = 'nav-mobile';
-document.querySelector('.nav').appendChild(mobile);
---
+++ Bulgarian/1.Homepage_bg.js
@@ -0,0 +1,3 @@
+var mobile = document.createElement('div');
+mobile.className = 'nav-mobile';
+document.querySelector('.nav').appendChild(mobile);
